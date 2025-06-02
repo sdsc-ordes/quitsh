25b091c89f00ab2fd8bdff5ca5b79862f812c3a8
Gabriel Nützi <gnuetzi@gmail.com>
2025-06-02 10:45:45 +0200

    fix: lint

--- pkg/cli/cmd/process-compose/exec/exec.go
+++ pkg/cli/cmd/process-compose/exec/exec.go
@@ -2,17 +2,12 @@ package processcomposeexec
 
 import (
 	"strings"
-	"time"
 
 	"github.com/sdsc-ordes/quitsh/pkg/cli"
 	"github.com/sdsc-ordes/quitsh/pkg/errors"
 	processcompose "github.com/sdsc-ordes/quitsh/pkg/exec/process-compose"
 	"github.com/spf13/cobra"
 )
-
-const longDesc = `Execute commands over process-compose on the right instance.`
-
-const timeoutContainers = 100 * time.Second
 
 type startArgs struct {
 	args []string
@@ -27,7 +22,6 @@ func AddCmd(cl cli.ICLI, parent *cobra.Command, defaultFlakeDir string) {
 	startCmd := &cobra.Command{
 		Use:     "exec [devenv-attr-path or devenv-installable] [args-to-proc-compose]",
 		Short:   "Exec commands on process-compose on the correct instance.",
-		Long:    longDesc,
 		PreRunE: cobra.MinimumNArgs(1),
 		RunE: func(_cmd *cobra.Command, args []string) error {
 			stArgs.attrPath = args[0]
