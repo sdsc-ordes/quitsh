812ab62b6584eaa0ab49d3c74572d64c902f4384
Gabriel Nützi <gnuetzi@gmail.com>
2025-06-08 11:03:20 +0200

    fix: add `.output` to path filter

--- pkg/filesystem/glob-option.go
+++ pkg/filesystem/glob-option.go
@@ -54,7 +54,7 @@ func WithPathFilter(f PathFilter, useAnd bool) FindOptions {
 
 // DefaultIgnoredDirectories returns all by default ignored directories.
 func IgnoredDirectoriesDefault() []string {
-	return []string{".git", ".direnv", ".devenv"}
+	return []string{".git", ".direnv", ".devenv", OutputDir}
 }
 
 // WithPathFilterDefault sets the default path filter if non it set.
