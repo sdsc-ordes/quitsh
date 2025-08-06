b736b257ab2bef83d6f4618102697f796d1e5aa8
Gabriel Nützi <gnuetzi@gmail.com>
2025-08-06 12:24:35 +0200

    fix:

--- pkg/exec/python/context.go
+++ pkg/exec/python/context.go
@@ -1,16 +1,21 @@
 package python
 
 import (
+	"path"
+
 	"github.com/sdsc-ordes/quitsh/pkg/exec"
 )
 
-// NewVEnvCtxBuilder creates a new command ctx buildter for a python virtual environment.
+// NewVEnvCtxBuilder creates a new command ctx builder for a python virtual environment.
 // It will enable path look up for passed commands to find the executable in the
-// modified env. `PATH` and adds `VIRTUAL_ENV` env. variable.
+// modified env. `PATH` and adds
+// `VIRTUAL_ENV` env. variable to point to the virtual env. dir.
 // NewCtx returns a new Go command context builder.
 func NewVEnvCtxBuilder(venvDir string, env []string) exec.CmdContextBuilder {
 	return exec.NewCmdCtxBuilder().
-		Env("VIRTUAL_ENV=" + venvDir).
+		Env(
+			"VIRTUAL_ENV=" + venvDir,
+		).
 		Env(env...).
-		Paths(venvDir)
+		Paths(path.Join(venvDir, "bin"))
 }
