70c32526f08c14d5cfb2620b488548111e6c4ad7
Gabriel Nützi <gnuetzi@gmail.com>
2025-08-06 12:19:43 +0200

    fix: add virtual env var.

--- pkg/exec/python/context.go
+++ pkg/exec/python/context.go
@@ -6,10 +6,11 @@ import (
 
 // NewVEnvCtxBuilder creates a new command ctx buildter for a python virtual environment.
 // It will enable path look up for passed commands to find the executable in the
-// modified env. `PATH`.
+// modified env. `PATH` and adds `VIRTUAL_ENV` env. variable.
 // NewCtx returns a new Go command context builder.
 func NewVEnvCtxBuilder(venvDir string, env []string) exec.CmdContextBuilder {
 	return exec.NewCmdCtxBuilder().
+		Env("VIRTUAL_ENV=" + venvDir).
 		Env(env...).
 		Paths(venvDir)
 }
