a5c923de854200d3f5c26f347a45c0af4ccac7d1
Gabriel Nützi <gnuetzi@gmail.com>
2026-03-18 09:44:04 +0100

    fix: improve SSH agent start/stop

--- pkg/exec/ssh/agent.go
+++ pkg/exec/ssh/agent.go
@@ -3,45 +3,122 @@ package ssh
 import (
 	"os"
 	"path"
+	"regexp"
+	"strconv"
 
 	"github.com/sdsc-ordes/quitsh/pkg/errors"
 	"github.com/sdsc-ordes/quitsh/pkg/exec"
+	"github.com/sdsc-ordes/quitsh/pkg/log"
 )
+
+type Agent struct {
+	SocketPath string
+	pid        int
+}
+
+type StartOption func(*opts)
+
+type opts struct {
+	newInstance bool
+}
+
+func (c *opts) Apply(options ...StartOption) {
+	for _, f := range options {
+		f(c)
+	}
+}
+
+// WithNewInstance recreats a new agent instead of testing if one already runs.
+func WithNewInstance(enable bool) StartOption {
+	return func(o *opts) {
+		o.newInstance = enable
+	}
+}
 
 // StartAgent starts the `ssh-agent` if its not yet running and returns
 // the socket path.
-func StartAgent() (socketPath string, err error) {
+func StartAgent(log log.ILog, options ...StartOption) (agent Agent, err error) {
+	var o opts
+	o.Apply(options...)
+
 	exeCtx := exec.NewCommandCtx(".")
-	start := false
+	newInstance := o.newInstance
 
-	err = exeCtx.CheckWithEC(func(cmdError *exec.CmdError) error {
-		if cmdError != nil && cmdError.ExitCode() == 2 {
-			start = true
+	if !newInstance {
+		// Check if already running.
+		err = exeCtx.CheckWithEC(func(cmdError *exec.CmdError) error {
+			if cmdError != nil && cmdError.ExitCode() == 2 {
+				log.Info("SSH agent is not running.")
 
-			return nil
+				// Not running start a new one.
+				newInstance = true
+
+				return nil
+			}
+
+			return cmdError
+		}, "ssh-add", "-l")
+		if err != nil {
+			return Agent{}, err
 		}
-
-		return cmdError
-	}, "ssh-add", "-l")
-	if err != nil {
-		return "", err
 	}
 
-	if start {
+	if newInstance {
+		log.Info("Start a new SSH agent.")
+
 		d, e := os.MkdirTemp("", "ssh-agent")
 		if e != nil {
-			return "", e
+			return Agent{}, errors.AddContext(e, "Could not start ssh-agent.")
 		}
 
 		f := path.Join(d, "socket")
 
-		return f, exeCtx.Check("ssh-agent", "-a", f)
+		out, e := exeCtx.Get("ssh-agent", "-a", f)
+		if e != nil {
+			return Agent{},
+				errors.AddContext(e, "Could not start ssh-agent.")
+		}
+		m := regexp.MustCompile(`SSH_AGENT_PID=(\d+)`).FindStringSubmatch(out)
+		if len(m) < 2 { //nolint:mnd
+			return Agent{},
+				errors.New("Could not extract PID from ssh-agent start.")
+		}
+
+		pid, e := strconv.Atoi(m[1])
+		if e != nil || pid == 0 {
+			return Agent{},
+				errors.AddContext(e, "Could not extract PID from ssh-agent start.")
+		}
+
+		log.Infof("Started SSH agent on socket '%v' and PID '%v'", f, pid)
+
+		return Agent{SocketPath: f, pid: pid}, nil
 	}
 
-	socketPath = os.Getenv("SSH_AUTH_SOCK")
+	socketPath := os.Getenv("SSH_AUTH_SOCK")
 	if socketPath == "" {
-		return "", errors.New("SSH_AUTH_SOCK is not defined, but should be since agent is running!")
+		return Agent{},
+			errors.New("SSH_AUTH_SOCK is not defined, but should be since agent is running!")
 	}
 
-	return socketPath, nil
+	return Agent{socketPath, 0}, nil
+}
+
+// Close closes the agent if started.
+func (c *Agent) Close() error {
+	if c.pid == 0 {
+		return nil
+	}
+
+	p, _ := os.FindProcess(c.pid)
+	if p == nil {
+		return nil
+	}
+
+	e := p.Kill()
+	if e != nil {
+		return errors.AddContext(e, "Could not kill ssh-agent with PID '%v'.", c.pid)
+	}
+
+	return nil
 }
--- pkg/exec/ssh/agent_test.go
+++ pkg/exec/ssh/agent_test.go
@@ -5,12 +5,19 @@ package ssh
 import (
 	"testing"
 
+	"github.com/sdsc-ordes/quitsh/pkg/log"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestAgent(t *testing.T) {
-	socketPath, err := StartAgent()
+	log := log.Global()
+	agent, err := StartAgent(log, WithNewInstance(true))
+
 	require.NoError(t, err)
-	assert.NotEmpty(t, socketPath)
+	assert.NotEmpty(t, agent.SocketPath)
+	assert.NotEmpty(t, agent.pid)
+
+	err = agent.Close()
+	require.NoError(t, err)
 }
